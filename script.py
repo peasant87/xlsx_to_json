4a49b91f8a4370fc95356069efb604e40c2e3331
Tiago Amaral <tamaral.info@gmail.com>
2021-06-03 03:20:46 -0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+MODEL_ID = 'merchant_product_model_id'
+CONFIG_ID = 'merchant_product_config_id'
+SIMPLE_ID = 'merchant_product_simple_id'
+
 def fill_dict(product_model_dict, list_sheets):
     for model in product_model_dict:
         product_config_dict = config_dict(model,list_sheets)
@@ -9,19 +13,19 @@ def fill_dict(product_model_dict, list_sheets):
         product_model_dict[model]['product_config'] = product_config_dict
 
 def config_dict(model_id,list_sheets):
-    product_config = list_sheets[1].set_index('merchant_product_model_id').loc[model_id]
-    product_config_dict = product_config.set_index('merchant_product_config_id').to_dict(orient='index')
+    product_config = list_sheets[1].set_index(MODEL_ID).loc[model_id]
+    product_config_dict = product_config.set_index(CONFIG_ID).to_dict(orient='index')
     return product_config_dict
 
 def media_simple_dict(product_config_dict,list_sheets):
-    media_df, product_simple_df = set_index('merchant_product_config_id',list_sheets)
+    media_df, product_simple_df = set_index(CONFIG_ID,list_sheets)
     
     for config in product_config_dict:
         df = media_df.loc[config]
         media_dict = df[df.columns[2:]].to_dict(orient='records')
         
         df = product_simple_df.loc[config]
-        product_simple_dict = df.set_index('merchant_product_simple_id').to_dict(orient='index')
+        product_simple_dict = df.set_index(SIMPLE_ID).to_dict(orient='index')
         
         product_config_dict[config]['media'] = media_dict
         product_config_dict[config]['product_simple'] = product_simple_dict
